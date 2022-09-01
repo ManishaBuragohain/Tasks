55f75b1cbc801020d79d1c7ddaa28f097706b992
gohain1996 <flowerdark777@gmail.com>
2022-09-01 15:26:59 +0530

    changes made

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,31 +1,70 @@
 <template>
-<h1>Task 3</h1>
-    <div>
-        <input type="text" placeholder="enter a text" v-model="name"> <br><br>
-        <button @click="getResult">Get Result</button>
+  <h2>Task 4</h2>
+          <form>
+            <div class="form-group">
+              <label for="todoitem">Add Datas :</label><br />
+              <input
+                type="text"
+                v-model="input"
+                class="form-control"
+                id="todoitem"
+                placeholder="add"
+              />
+            </div>
+            <br /><br />
+            <button type="button" v-on:click="add()" class="storebtn">
+              Store
+            </button>
+          </form>
+       
+   
+        <h2>Results</h2>
+        <div class="list-group">
+          <p
+            v-for="(todo, index) in todos"
+            :key="index"
+            class="list-group-item"
+          >
+            {{ todo }}
+          </p>
+        </div>
+    
 
-        
-
-    </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                name : "",
-            };
-        },
-        methods:{
-            getResult(){
-                alert(this.name);
-            }
-
-        }
-        
-    }
+export default {
+  name: "Todo",
+  data() {
+    return {
+      todos: [],
+      input: "",
+    };
+  },
+  methods: {
+    add() {
+    //   this.todos.push(this.input);
+    //   this.input = "";
+        this.todos.push(this.input);
+       this.todos = [ ...new Set(this.todos) ]
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style>
+.storebtn {
+  width: 80px;
+}
 
+input[type="text"],
+select {
+  width: 20%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
 </style>
